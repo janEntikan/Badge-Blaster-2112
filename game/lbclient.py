20b52263ed47caad928afae56c92bdfd25dc259c
Tiziano Bettio <tc@tizilogic.com>
2021-04-03 02:01:18 +0200

    Improve documentation

--- game/lbclient.py
+++ game/lbclient.py
@@ -8,11 +8,16 @@ class LeaderBoard:
         self._lb = []
 
     def leaderboard(self):
+        """If able to connect to the server, returns a list of 10 tuple(name, score)."""
         if not self._lb:
             self._update_lb()
         return self._lb
 
     def submit(self, name, score):
+        """
+        Submit a score to the leaderboard. Returns True if communication was successful.
+        True does not mean that the score made it into the top 10!!!
+        """
         reply = self._send_msg(f'{chr(99)}{name}{score}'.encode())
         self._lb = []  # Force update on next read
         if reply[0] == 1:
@@ -34,5 +39,5 @@ class LeaderBoard:
             s.close()
         except (OSError, ConnectionAbortedError, ConnectionError, ConnectionRefusedError) as err:
             print(f'Something went wrong: {err}')
-            return b''
+            return b'\0'
         return data
