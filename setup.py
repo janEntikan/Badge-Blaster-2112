888d99c8b4056cf2be6a45dc7031d1e2246a320b
rdb <git@rdb.name>
2021-04-03 23:07:55 +0200

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name="Badge Blaster",
+    options={
+        'build_apps': {
+            'include_patterns': [
+                'assets/cache/*',
+                'assets/fireworks/*.png',
+                'assets/fonts/*.ttf',
+                'assets/gui/*.png',
+                'assets/models/*.bam',
+                'assets/music/*.ogg',
+                'assets/sfx/*.wav',
+                'assets/shaders/*.vert',
+                'assets/shaders/*.frag',
+                'README.md',
+                'keybindings.toml',
+                'settings.prc',
+                'assets/cache/*',
+            ],
+            'gui_apps': {
+                'bdgeblst': 'main.py',
+            },
+            'log_filename': '$USER_APPDATA/Badge Blaster/output.log',
+            'log_append': False,
+            'plugins': [
+                'pandagl',
+                'p3openal_audio',
+            ],
+        }
+    }
+)
